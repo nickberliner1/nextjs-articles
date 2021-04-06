4d625cd28f8d10ffc905a484b695f20ac7df54b8
Nick Berliner <nick.berliner96@gmail.com>
2021-04-06 17:08:52 +0200

    Navbar created

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,10 @@
+import Nav from './Nav';
 import styles from '../styles/Layout.module.css';
 
 const Layout = ({ children }) => {
     return (
         <>
+            <Nav />
             <div className={styles.container}>
                 <main className={styles.main}>
                     {children}
