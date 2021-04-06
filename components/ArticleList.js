3dda213a848cb23c8538576a0b5bcf59595eb2fc
Nick Berliner <nick.berliner96@gmail.com>
2021-04-06 17:53:21 +0200

    Article Item created and styled

--- components/ArticleList.js
+++ components/ArticleList.js
@@ -1,11 +1,13 @@
+import ArticleItem from './ArticleItem';
+
 import articleStyles from '../styles/Article.module.css';
 
-const ArticleList = ({articles}) => {
+const ArticleList = ({ articles }) => {
     return (
         <div className={articleStyles.grid}>
-            {
-            articles.map(article => <h3>{article.title}</h3>)
-            }
+            {articles.map((article) => (
+                <ArticleItem article={article} />
+            ))}
         </div>
     )
 }
