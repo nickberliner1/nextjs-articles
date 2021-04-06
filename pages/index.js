bb5b42c6da4cac2c93e1c7ef9fefd4b5e3b0c2dc
Nick Berliner <nick.berliner96@gmail.com>
2021-04-06 17:42:53 +0200

    articles displayed

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,30 @@
 import Head from 'next/head'
 import styles from '../styles/Layout.module.css'
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>WebDev News</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="keywords" content='web development, programming, react, next' />
-      </Head>
-      <h1>Welcome to NextJS</h1>
-    </div>
-  )
+export default function Home({articles}) {
+	return (
+		<div className={styles.container}>
+		<Head>
+			<title>WebDev News</title>
+			<link rel="icon" href="/favicon.ico" />
+			<meta name="keywords" content='web development, programming, react, next' />
+		</Head>
+
+		{
+		articles.map(article => <h3>{article.title}</h3>)
+		}
+
+		</div>
+	)
+}
+
+export const getStaticProps = async () => {
+	const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+	const articles = await res.json()
+
+	return {
+		props: {
+			articles
+		}
+	}
 }
